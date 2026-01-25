1e0160807b15746c2ba433d1782a936556e79a33
MTeguhHermawan <mohamadhermawan37@admin.smp.belajar.id>
2026-01-25 19:32:09 +0700

    Update pengumuman.js

--- pengumuman.js
+++ pengumuman.js
@@ -1,13 +1,18 @@
 const pengumuman = [
   {
-    judul: "Masuk Sekolah Setelah Libur",
-    tanggal: "8 Januari 2026",
-    isi: "Siswa kelas VI masuk kembali seperti biasa setelah libur semester."
+    judul: "Isra Miraj 1447 H",
+    tanggal: "28 Januari 2026",
+    isi: "Kegiatan peringatan Isra Miraj 1447 H, di kampus SDN Sindang II."
   },
   {
-    judul: "Kegiatan Jumat Bersih",
-    tanggal: "10 Januari 2026",
-    isi: "Siswa diminta membawa alat kebersihan untuk kegiatan Jumat Bersih."
+    judul: "Sosialisasi TKA",
+    tanggal: "27 Januari 2026",
+    isi: "Rapat bersama orang tua / wali asuh membahas informasi dan teknis mengenai TKA (Tes Kemampuan Akademik) Pukul 09.00 WIB bertempat di ruang kelas 2."
+  },
+   {
+    judul: "Lini Masa Pelaksanaan TKA SD/MI/SDLB/Paket A/sederajat",
+    tanggal: "19 Januari - 24 Mei 2026",
+    isi: "Pendaftaran 19 Januari - 28 Febuari, Simulasi 2 - 8 Maret, Gladi Bersih 9 - 17 Maret, Pelaksanaan Utama 20 - 30 April, Pelaksanaan Susulan 11 - 17 Mei, Pengolahan Hasil 18 - 23 Mei, Pengumuman Hasil 24 Mei."
   }
 ];
 
